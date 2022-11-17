98b651ce88f343f51cd8edc09537835c9d96870c
Daniela Rosa <dam1perola@gmail.com>
2022-11-17 19:34:56 -0300

    Styling the project

---
+++ script.js
@@ -0,0 +1,15 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+    img.style.transform = `translateX($(-idx * 1080)px)`;
+}
+
+setInterval(carrossel, 1800);
